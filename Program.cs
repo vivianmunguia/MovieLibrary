74a73acc082ae7761118368134b8ff29ad2c0f14
Vivian Munguía <andy_munguia@hotmail.com>
2021-03-05 12:30:20 -0700

    Inicialización del repositorio

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MovieLibrary
+{
+
+    public class AplicacionPeliculas 
+    {
+        public string nombre;
+        public string URL;
+        public string logo;
+        public string dueña;
+        void AbrirAplicacion()
+        {
+            Console.WriteLine("Bienvenido a la aplicación");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aplicacionPeliculas = new AplicacionPeliculas(); 
+            aplicacionPeliculas.nombre = "Flick Fridays";
+            aplicacionPeliculas.URL = "www.flickfridays.com";
+            aplicacionPeliculas.logo = "logo.png";
+
+        }
+    }
+}
